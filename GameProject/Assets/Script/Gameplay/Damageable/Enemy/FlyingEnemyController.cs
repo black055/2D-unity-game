0d6dd4a1c5844a2cf03d38b059c33cf0122d0644
T N <truong1320vv@gmail.com>
2022-01-10 16:50:00 +0700

    Fix flying mob controller

--- GameProject/Assets/Script/Gameplay/Damageable/Enemy/FlyingEnemyController.cs
+++ GameProject/Assets/Script/Gameplay/Damageable/Enemy/FlyingEnemyController.cs
@@ -23,19 +23,18 @@ public class FlyingEnemyController : MonoBehaviour
   private float nextTimeAttack = 0f;
   private float currentHealth;
   private float KnockbackEnd;
+  private int facingDirection;
+  private float targetPositionX;
   private int attackLeft;
   private int currentWaypoint;
 
   private bool knockback;
-  private bool isNearCorner;
-  private bool isTouchingWall;
-  private bool isGrounded;
   private bool isAttacking;
   private bool isChasing;
   private bool isReturning;
 
   [SerializeField]
-  private Transform wallCheck, groundCheck, cornerCheck, damageCheck, attackCheck, startingPoint, target;
+  private Transform damageCheck, attackCheck, startingPoint;
   [SerializeField]
   private LayerMask groundLayer, knightLayer;
 
@@ -56,6 +55,7 @@ public class FlyingEnemyController : MonoBehaviour
     rbAlive = aliveObject.GetComponent<Rigidbody2D>();
     animator = aliveObject.GetComponent<Animator>();
     seeker = gameObject.GetComponent<Seeker>();
+    facingDirection = 1;
   }
 
   private void Update()
@@ -67,14 +67,27 @@ public class FlyingEnemyController : MonoBehaviour
         knockback = false;
         rbAlive.velocity = new Vector2(knockbackSpeedX, knockbackSpeedY);
       }
+      if (knightController != null)
+      {
+        if (knightController.gameObject.layer == LayerMask.NameToLayer("Dead"))
+        {
+          isReturning = true;
+          // 1 sec delay
+          Invoke("FindNewTarget", 5f + 1f);
+        }
+      }
 
       Chase();
       Patrol();
+      FlipX();
 
-      CheckSurroundings();
       CheckAttackRange();
-      FlipTransform();
     }
+  }
+
+  private void FindNewTarget()
+  {
+    knightController = GameObject.FindGameObjectWithTag("Knight").GetComponent<KnightController>();
   }
 
   private void OnPathComplete(Path p)
@@ -93,18 +106,8 @@ public class FlyingEnemyController : MonoBehaviour
     if (currentWaypoint >= path.vectorPath.Count)
       return;
 
-    if (isChasing)
-    {
+    if (isChasing || isReturning)
       animator.SetBool("isMoving", true);
-      animator.SetFloat("moveX", knightController.transform.position.x - rbAlive.position.x);
-      animator.SetFloat("moveY", knightController.transform.position.y - rbAlive.position.y);
-    }
-    else if (isReturning)
-    {
-      animator.SetBool("isMoving", true);
-      animator.SetFloat("moveX", startingPoint.position.x - rbAlive.position.x);
-      animator.SetFloat("moveY", startingPoint.position.y - rbAlive.position.y);
-    }
 
     rbAlive.transform.position = Vector2.MoveTowards(rbAlive.transform.position, path.vectorPath[currentWaypoint], moveSpeed * Time.deltaTime);
 
@@ -115,6 +118,7 @@ public class FlyingEnemyController : MonoBehaviour
 
   private void StartDrawChasingPath()
   {
+    if (knightController == null) return;
     int onLeftSide = aliveObject.transform.position.x > knightController.transform.position.x ? -1 : 1;
     Vector3 relativeCloseToKnightPos = knightController.transform.position + .6f * (onLeftSide * Vector3.left) + .3f * (Vector3.up);
     if (seeker.IsDone())
@@ -129,9 +133,14 @@ public class FlyingEnemyController : MonoBehaviour
 
   private void Chase()
   {
+    if (knightController == null) return;
     // Return back to the start spot
-    if (knightController && (isReturning || Vector2.Distance(aliveObject.transform.position, knightController.transform.position) > maxFollowDistance))
-    { isReturning = true; ReturnStartPoint(); return; }
+    if (isReturning || Vector2.Distance(aliveObject.transform.position, knightController.transform.position) > maxFollowDistance)
+    {
+      isReturning = true;
+      ReturnStartPoint();
+      return;
+    }
 
     if (!isChasing) return;
 
@@ -161,41 +170,23 @@ public class FlyingEnemyController : MonoBehaviour
     isChasing = chase;
   }
 
-  private void FlipTransform()
+  private void FlipX()
   {
     if (knightController == null) return;
-    bool isOnLeft = aliveObject.transform.position.x > knightController.transform.position.x;
 
-    if ((attackCheck.localPosition.x < 0 && isOnLeft) || (attackCheck.localPosition.x > 0 && !isOnLeft)) return;
+    if (isChasing)
+      targetPositionX = knightController.transform.position.x;
+    else if (isReturning)
+      targetPositionX = startingPoint.transform.position.x;
+    else return;
 
-    // wallCheck, groundCheck, cornerCheck, damageCheck, attackCheck
-    Vector3 newWallCheck = wallCheck.localPosition;
-    Vector3 newGroundCheck = groundCheck.localPosition;
-    Vector3 newCornerCheck = cornerCheck.localPosition;
-    Vector3 newDamageCheck = damageCheck.localPosition;
-    Vector3 newAttackCheck = attackCheck.localPosition;
+    bool isOnLeft = aliveObject.transform.position.x > targetPositionX;
+    if ((facingDirection == -1 && isOnLeft) || (facingDirection == 1 && !isOnLeft)) return;
 
 
-    newWallCheck.x *= -1;
-    newGroundCheck.x *= -1;
-    newCornerCheck.x *= -1;
-    newDamageCheck.x *= -1;
-    newAttackCheck.x *= -1;
-
-    wallCheck.localPosition = newWallCheck;
-    groundCheck.localPosition = newGroundCheck;
-    cornerCheck.localPosition = newCornerCheck;
-    damageCheck.localPosition = newDamageCheck;
-    attackCheck.localPosition = newAttackCheck;
+    facingDirection *= -1;
+    rbAlive.transform.Rotate(0f, 180f, 0f);
   }
-
-  private void CheckSurroundings()
-  {
-    isNearCorner = Physics2D.Raycast(cornerCheck.position, aliveObject.transform.up * -1f, groundCheckDistance, groundLayer);
-    isTouchingWall = Physics2D.Raycast(wallCheck.position, aliveObject.transform.right, groundCheckDistance, groundLayer);
-    isGrounded = Physics2D.Raycast(groundCheck.position, aliveObject.transform.up * -1f, groundCheckDistance, groundLayer);
-  }
-
 
   private void Damage(float[] attackDetails)
   {
@@ -204,8 +195,7 @@ public class FlyingEnemyController : MonoBehaviour
 
     Instantiate(hitEffect, aliveObject.transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
 
-    bool isOnLeft = aliveObject.transform.position.x > knightController.transform.position.x;
-    animator.SetTrigger("hurt" + (isOnLeft ? "Left" : "Right"));
+    animator.SetTrigger("hurt");
 
     if (currentHealth > 0.0f)
     {
@@ -242,9 +232,7 @@ public class FlyingEnemyController : MonoBehaviour
       if (isAttacking)
       {
         attackLeft = numAttackTime;
-
-        bool isOnLeft = aliveObject.transform.position.x > knightController.transform.position.x;
-        animator.SetTrigger("attack" + (isOnLeft ? "Left" : "Right"));
+        animator.SetTrigger("attack");
       }
     }
   }
@@ -262,7 +250,6 @@ public class FlyingEnemyController : MonoBehaviour
     if (attackLeft == 0)
     {
       nextTimeAttack = Time.time + attackCooldown;
-      Debug.Log(nextTimeAttack);
       isAttacking = false;
     }
   }
@@ -270,9 +257,6 @@ public class FlyingEnemyController : MonoBehaviour
   private void OnDrawGizmos()
   {
     Gizmos.DrawWireSphere(attackCheck.position, attackRange);
-    Gizmos.DrawLine(wallCheck.position, new Vector3(wallCheck.position.x + groundCheckDistance, wallCheck.position.y, wallCheck.position.z));
-    Gizmos.DrawLine(cornerCheck.position, new Vector3(cornerCheck.position.x, cornerCheck.position.y - groundCheckDistance, cornerCheck.position.z));
-    Gizmos.DrawWireSphere(groundCheck.position, groundCheckDistance);
 
   }
 }
